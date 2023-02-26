524c42ae1f02f8f6b5430a59de23400f0a0e14d1
SakunthaMV <e17340@eng.pdn.ac.lk>
2023-02-27 03:13:50 +0530

    designed homepage fully without adding functions

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,29 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:number_crush/Screens/Widgets/common_appbar.dart';
+import 'package:number_crush/Screens/Widgets/common_background.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const CommonAppBar(),
-      body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: const [
-            Icon(
-              Icons.star,
-              size: 35.0,
-              color: Color(0xFFB3B334),
-            ),
-            Icon(
-              Icons.star,
-              size: 30,
-              color: Color(0xFFFFFF00),
-            ),
+    final double width = MediaQuery.of(context).size.width;
+    const int level = 1;
+    const String backgroundStyle = 'Classic';
+    return CommonBackground(
+      content: Padding(
+        padding: EdgeInsets.symmetric(horizontal: width * 0.1),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            homeRow(context, Icons.play_arrow, 'PLAY',
+                '${level < 2 ? 'Start' : 'Continue'} Your Journey'),
+            homeRow(context, Icons.event, 'STAGES', 'Currunt Level: $level'),
+            homeRow(context, Icons.wallpaper, 'BACKGROUNDS', 'Currunt: $backgroundStyle')
           ],
         ),
+      ),
+    );
+  }
+
+  Widget homeRow(BuildContext context, IconData icon, String topic, String subTopic) {
+    final double width = MediaQuery.of(context).size.width;
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        children: [
+          SizedBox(
+            width: width * 0.2,
+            height: width * 0.2,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: colorScheme.secondary,
+              ),
+              child: Icon(
+                icon,
+                size: 40,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: width * 0.05),
+            child: FittedBox(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    topic,
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  Text(
+                    subTopic,
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: colorScheme.onSecondary,
+                        ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
