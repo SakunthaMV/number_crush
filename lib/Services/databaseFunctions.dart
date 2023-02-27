20005bb72906c256bf25777db69c9fc5356ac959
thamindumk <thamindu.20@cse.mrt.ac.lk>
2023-02-27 14:59:11 +0530

    done database function class

--- lib/Services/databaseFunctions.dart
+++ lib/Services/databaseFunctions.dart
@@ -1,5 +1,4 @@
 import 'package:sqflite/sqflite.dart';
-
 import 'DatabaseHelper.dart';
 
 class DatabaseFunctions {
@@ -9,5 +8,10 @@ class DatabaseFunctions {
     Database db = await dbHelper.database;
     return await db
         .rawInsert(''' INSERT INTO stage(toUnlock,stars) VALUES(100,20) ''');
+  }
+
+  Future qu(table) async {
+    Database db = await dbHelper.database;
+    return await db.rawQuery(''' SELECT * FROM stage ''');
   }
 }
