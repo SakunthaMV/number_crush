7618817d30ad74a6c80e92e388d10a43e33c3edf
SakunthaMV <e17340@eng.pdn.ac.lk>
2023-03-02 23:51:42 +0530

    add size and color parameters to star.

--- lib/Screens/Widgets/Stars/star.dart
+++ lib/Screens/Widgets/Stars/star.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 
 class Star extends StatelessWidget {
   final double size;
-  const Star({super.key, this.size = 30.0});
+  final Color color;
+  const Star({super.key, this.size = 30.0, this.color = const Color(0xFFFFFF00)});
 
   @override
   Widget build(BuildContext context) {
     return Transform.scale(
-      scale: (1.0 / 30.0) * size,
+      scale: (1.0 / 25.0) * size,
       child: Stack(
         alignment: Alignment.center,
         children: [
@@ -18,8 +19,8 @@ class Star extends StatelessWidget {
           ),
           Icon(
             Icons.star,
-            size: 30,
-            color: Theme.of(context).colorScheme.outlineVariant,
+            size: 25.0,
+            color: color,
           ),
         ],
       ),
--- lib/Screens/Widgets/Stars/stars_row.dart
+++ lib/Screens/Widgets/Stars/stars_row.dart
@@ -3,19 +3,37 @@ import 'package:number_crush/Screens/Widgets/Stars/star.dart';
 
 class StarsRow extends StatelessWidget {
   final int amount;
-  const StarsRow({super.key, this.amount = 0});
+  final Color color;
+  final double size;
+  const StarsRow({
+    super.key,
+    this.amount = 0,
+    this.color = const Color(0xFFFFFF00),
+    this.size = 20.0,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        const Star(size: 20),
-        Text(
-          '$amount',
-          style: Theme.of(context).textTheme.titleLarge,
-        ),
-      ],
+    return SizedBox(
+      width: size * 3.2,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Star(
+            size: size,
+            color: color,
+          ),
+          Text(
+            '$amount',
+            style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                  fontSize: size,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+          ),
+        ],
+      ),
     );
   }
 }
