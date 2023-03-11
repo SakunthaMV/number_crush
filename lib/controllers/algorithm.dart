d789224cd52a1ecd58247f02ce386d612e74fd94
thamindumk <thamindu.20@cse.mrt.ac.lk>
2023-03-11 12:39:32 +0530

    answer generator

--- lib/controllers/algorithm.dart
+++ lib/controllers/algorithm.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:number_crush/Models/Question.dart';
+
 class Algorithm {
   Random random = Random();
 
@@ -42,7 +44,7 @@ class Algorithm {
     },
     7: {
       'operators': ['+', '-', '*', '/', 'squre', 'squreRoot', 'log'],
-      'maxOparand': [10000, 100, 100, 100],
+      'maxOparand': [10000, 100, 100, 10],
       'maxAnsDigit': 5,
       'numberOfQuestion': 5
     },
@@ -70,30 +72,61 @@ class Algorithm {
     return -1;
   }
 
-  int _questionRange(int questionNumber) {
-    return 0 < questionNumber && questionNumber < 11
+  double _timeForQuesion(int type, int size_1, int size_2) {
+    double scale = 1.9;
+    switch (type) {
+      case 0:
+        {
+          int t = 1 + size_1 * size_2 * 1;
+          return t / scale;
+        }
+      case 1:
+        {
+          int t = 2 + size_1 * size_2 * 4;
+          return t / scale;
+        }
+      case 2:
+        {
+          int t = 3 + size_1 * size_2 * 3;
+          return t / scale;
+        }
+      case 3:
+        {
+          int t = 2 + size_1 * size_2 * 2;
+          return t / scale;
+        }
+    }
+    return 0.0;
+  }
+
+  int _questionRange(int level) {
+    return 0 < level && level < 11
         ? 1
-        : 10 < questionNumber && questionNumber < 51
+        : 10 < level && level < 51
             ? 2
-            : 50 < questionNumber && questionNumber < 101
+            : 50 < level && level < 101
                 ? 3
-                : 100 < questionNumber && questionNumber < 201
+                : 100 < level && level < 201
                     ? 4
-                    : 200 < questionNumber && questionNumber < 501
+                    : 200 < level && level < 501
                         ? 5
-                        : 500 < questionNumber && questionNumber < 1001
+                        : 500 < level && level < 1001
                             ? 6
-                            : 1000 < questionNumber && questionNumber < 2001
+                            : 1000 < level && level < 2001
                                 ? 7
                                 : 8;
   }
 
-  List<dynamic> _questionGenerator(Map<String, dynamic> dataMap) {
+  Question _questionGenerator(Map<String, dynamic> dataMap) {
     String operator =
         dataMap['operators'][random.nextInt(dataMap['operators'].length)];
-    int operandDigit = _findTypeOfOperator(_operatorList, operator);
-    int operand_1 = random.nextInt(dataMap['maxOparand'][operandDigit]);
-    int operand_2 = random.nextInt(dataMap['maxOparand'][operandDigit]);
+    int operatorType = _findTypeOfOperator(_operatorList, operator);
+    int operandDigit = dataMap['maxOparand'][operatorType];
+    int operand_1 = random.nextInt(operandDigit);
+    int operand_2 = random.nextInt(operandDigit);
+    double t = double.parse((_timeForQuesion(
+            operatorType, '$operand_1'.length, '$operand_2'.length))
+        .toStringAsFixed(2));
     int result = 0;
     if (operator == '+') {
       result = operand_1 + operand_2;
@@ -111,8 +144,8 @@ class Algorithm {
     } else if (operator == '/') {
       result = operand_1 * operand_2;
       int temp = result;
-      operand_1 = result;
-      result = temp;
+      result = operand_1;
+      operand_1 = temp;
     } else if (operator == 'squre') {
       result = pow(operand_1, 2) as int;
     } else if (operator == 'squreRoot') {
@@ -133,103 +166,156 @@ class Algorithm {
       result = operand_2;
       operand_2 = temp;
     }
-
-    List<dynamic> questions = [operand_1, operator, operand_2, result];
-    return questions;
+    Map<String, dynamic> map = {
+      'operator': operator,
+      'operand_1': operand_1,
+      'operand_2': operand_2,
+      'correctAns': result,
+      'time': t,
+    };
+    Question question = Question.fromMap(map);
+    return question;
   }
 
-  Map<int, List<dynamic>> dataList(int questionNumber) {
-    Map<int, List<dynamic>> questionsMap = {};
-    switch (_questionRange(questionNumber)) {
+  List<Question> questionList(int level) {
+    List<Question> questionList = [];
+    switch (_questionRange(level)) {
       case 1:
         {
           int numberOfQuestion = 2;
           for (int i = 0; i < numberOfQuestion; i++) {
-            List<dynamic> question = _questionGenerator(_data[1]);
-            questionsMap[i + 1] = question;
+            Question question = _questionGenerator(_data[1]);
+            question.levelId = level;
+            question = _answerGenerator(question);
+            questionList.add(question);
           }
-          return questionsMap;
+          return questionList;
         }
       case 2:
         {
           int numberOfQuestion =
               2 + random.nextInt(_data[2]['numberOfQuestion'] - 1);
           for (int i = 0; i < numberOfQuestion; i++) {
-            List<dynamic> question = _questionGenerator(_data[2]);
-            questionsMap[i + 1] = question;
+            Question question = _questionGenerator(_data[2]);
+            question.levelId = level;
+            question = _answerGenerator(question);
+            questionList.add(question);
           }
-          return questionsMap;
+          return questionList;
         }
       case 3:
         {
           int numberOfQuestion =
               2 + random.nextInt(_data[3]['numberOfQuestion'] - 1);
           for (int i = 0; i < numberOfQuestion; i++) {
-            List<dynamic> question = _questionGenerator(_data[3]);
-            questionsMap[i + 1] = question;
+            Question question = _questionGenerator(_data[3]);
+            question.levelId = level;
+            question = _answerGenerator(question);
+            questionList.add(question);
           }
-          return questionsMap;
+          return questionList;
         }
       case 4:
         {
           int numberOfQuestion =
               2 + random.nextInt(_data[4]['numberOfQuestion'] - 1);
           for (int i = 0; i < numberOfQuestion; i++) {
-            List<dynamic> question = _questionGenerator(_data[4]);
-            questionsMap[i + 1] = question;
+            Question question = _questionGenerator(_data[4]);
+            question.levelId = level;
+            question = _answerGenerator(question);
+            questionList.add(question);
           }
-          return questionsMap;
+          return questionList;
         }
       case 5:
         {
           int numberOfQuestion =
               2 + random.nextInt(_data[5]['numberOfQuestion'] - 1);
           for (int i = 0; i < numberOfQuestion; i++) {
-            List<dynamic> question = _questionGenerator(_data[5]);
-            questionsMap[i + 1] = question;
+            Question question = _questionGenerator(_data[5]);
+            question.levelId = level;
+            question = _answerGenerator(question);
+            questionList.add(question);
           }
-          return questionsMap;
+          return questionList;
         }
       case 6:
         {
           int numberOfQuestion =
               2 + random.nextInt(_data[6]['numberOfQuestion'] - 1);
           for (int i = 0; i < numberOfQuestion; i++) {
-            List<dynamic> question = _questionGenerator(_data[6]);
-            questionsMap[i + 1] = question;
+            Question question = _questionGenerator(_data[6]);
+            question.levelId = level;
+            question = _answerGenerator(question);
+            questionList.add(question);
           }
-          return questionsMap;
+          return questionList;
         }
       case 7:
         {
           int numberOfQuestion =
               2 + random.nextInt(_data[7]['numberOfQuestion'] - 1);
           for (int i = 0; i < numberOfQuestion; i++) {
-            List<dynamic> question = _questionGenerator(_data[7]);
-            questionsMap[i + 1] = question;
+            Question question = _questionGenerator(_data[7]);
+            question.levelId = level;
+            question = _answerGenerator(question);
+            questionList.add(question);
           }
-          return questionsMap;
+          return questionList;
         }
       case 8:
         {
           int numberOfQuestion =
               2 + random.nextInt(_data[8]['numberOfQuestion'] - 1);
           for (int i = 0; i < numberOfQuestion; i++) {
-            List<dynamic> question = _questionGenerator(_data[8]);
-            questionsMap[i + 1] = question;
+            Question question = _questionGenerator(_data[8]);
+            question.levelId = level;
+            question = _answerGenerator(question);
+            questionList.add(question);
           }
-          return questionsMap;
+          return questionList;
         }
       default:
         {
           int numberOfQuestion =
               2 + random.nextInt(_data[8]['numberOfQuestion'] - 1);
           for (int i = 0; i < numberOfQuestion; i++) {
-            List<dynamic> question = _questionGenerator(_data[8]);
-            questionsMap[i + 1] = question;
+            Question question = _questionGenerator(_data[8]);
+            question.levelId = level;
+            question = _answerGenerator(question);
+            questionList.add(question);
           }
-          return questionsMap;
+          return questionList;
         }
     }
+  }
+
+  Question _answerGenerator(Question question) {
+    int result = question.correctAns;
+    Set<int> answerSet = {};
+    if ('$result'.length < 2) {
+      while (answerSet.length != 3) {
+        int ans = random.nextInt(10);
+        if (ans == result) {
+          continue;
+        }
+        answerSet.add(ans);
+      }
+      List<int> answer = answerSet.toList();
+      question.setAnswer(answer);
+    } else {
+      while (answerSet.length != 3) {
+        int ans =
+            (result - result * 0.15 + random.nextInt((result * 0.3).round()))
+                .round();
+        if (ans == result) {
+          continue;
+        }
+        answerSet.add(ans);
+      }
+      List<int> answer = answerSet.toList();
+      question.setAnswer(answer);
+    }
+    return question;
   }
 }
