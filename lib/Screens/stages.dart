739e3f8ac3ef72523e7ab885e8765e4dba028368
SakunthaMV <e17340@eng.pdn.ac.lk>
2023-03-13 14:29:09 +0530

    totally change stage card

--- lib/Screens/stages.dart
+++ lib/Screens/stages.dart
@@ -1,5 +1,3 @@
-import 'dart:ui' as ui;
-
 import 'package:flutter/material.dart';
 import 'package:number_crush/Screens/Widgets/Stars/stars_row.dart';
 import 'package:number_crush/Screens/Widgets/common_background.dart';
@@ -13,24 +11,26 @@ class Stages extends StatelessWidget {
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
     return CommonBackground(
-      extend: false,
-      content: ListView.builder(
+      content: Padding(
         padding: EdgeInsets.symmetric(
           horizontal: width * 0.05,
           vertical: width * 0.025,
         ),
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          return _stageContainer(context, index);
-        },
+        child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return _stageContainer(context, index);
+          },
+        ),
       ),
     );
   }
 
-  Container _stageContainer(BuildContext context, int index, {int stars = 0}) {
+  Container _stageContainer(BuildContext context, int index, {int stars = 5}) {
     final double width = MediaQuery.of(context).size.width;
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
     final TextTheme textTheme = Theme.of(context).textTheme;
+    final bool locked = index == 4;
     return Container(
       height: 250,
       margin: EdgeInsets.symmetric(vertical: width * 0.02),
@@ -46,93 +46,73 @@ class Stages extends StatelessWidget {
         highlightColor: Theme.of(context).splashColor,
         child: Card(
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(18.0),
+            borderRadius: BorderRadius.circular(20.0),
           ),
-          color: colorScheme.primaryContainer,
+          color: colorScheme.outline,
           elevation: 5,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(18.0),
-            child: Stack(
-              alignment: Alignment.centerRight,
-              children: [
-                CustomPaint(
-                  painter: StageShape(),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: width * 0.1),
-                      child: Opacity(
-                        opacity: 0.2,
-                        child: Text(
-                          '${index + 1}',
-                          style: textTheme.displayLarge?.copyWith(
-                            color: colorScheme.onBackground,
-                          ),
-                        ),
-                      ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    'STAGE',
+                    style: textTheme.displaySmall!.copyWith(
+                      color: colorScheme.primary,
+                      height: 0.8,
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(right: width * 0.05),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      StarsRow(
-                        size: 30.0,
-                        amount: stars,
-                      ),
-                      StarsRow(
-                        size: 30.0,
-                        amount: 150,
-                        color: colorScheme.primaryContainer,
-                      ),
-                      const SizedBox(
-                        height: 30.0,
-                      ),
-                      Text(
-                        'STAGE ${index + 1}',
-                        style: textTheme.displayLarge?.copyWith(
-                          fontSize: 30.0,
-                          color: colorScheme.primary,
+                  Text(
+                    '${index + 1}',
+                    style: textTheme.displayMedium!.copyWith(
+                      color: colorScheme.primary,
+                      height: 0.7,
+                    ),
+                  ),
+                  if (!locked)
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        StarsRow(
+                          size: 30,
+                          starBoder: colorScheme.tertiary,
+                          borderSize: 5.0,
+                          amount: stars,
                         ),
-                      )
-                    ],
+                        StarsRow(
+                          size: 30,
+                          color: colorScheme.outline,
+                          starBoder: colorScheme.primary,
+                          borderSize: 5.0,
+                          amount: 150 - stars,
+                        ),
+                      ],
+                    )
+                  else
+                    StarsRow(
+                      size: 30,
+                      color: colorScheme.outline,
+                      starBoder: colorScheme.tertiary,
+                      borderSize: 5.0,
+                      amount: 135,
+                    ),
+                ],
+              ),
+              if (locked)
+                Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context)
+                        .appBarTheme
+                        .backgroundColor!
+                        .withOpacity(0.6),
+                    borderRadius: BorderRadius.circular(20.0),
                   ),
                 ),
-              ],
-            ),
+            ],
           ),
         ),
       ),
     );
-  }
-}
-
-class StageShape extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint();
-    paint.shader = ui.Gradient.linear(
-      Offset(0.0, size.height),
-      Offset(size.width * 0.7, 0.0),
-      [const Color(0xFFA5A4C2), const Color(0xFF5A5261)],
-    );
-    paint.style = PaintingStyle.fill;
-    var path = Path();
-
-    path.lineTo(18.0, 0.0);
-    path.lineTo(size.width * 0.32, 35.0);
-    path.lineTo(size.width * 0.7, 0.0);
-    path.lineTo(size.width * 0.32, size.height);
-    path.lineTo(0.0, size.height);
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
   }
 }
