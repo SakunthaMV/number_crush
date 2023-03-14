45a1e7b57d8f3467d77f7388b51df0398f01bbbb
SakunthaMV <e17340@eng.pdn.ac.lk>
2023-03-15 00:56:09 +0530

    get relevent data from the database

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,40 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:number_crush/Screens/Widgets/common_background.dart';
+import 'package:number_crush/Screens/stage_home.dart';
 import 'package:number_crush/Screens/stages.dart';
+import 'package:number_crush/Services/databaseFunctions.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
+  Future<int> _currnetLevel() async {
+    DatabaseFunctions db = DatabaseFunctions();
+    return await db.lastUnlockLevel();
+  }
+
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
-    const int level = 1;
     const String backgroundStyle = 'Classic';
     return CommonBackground(
       content: Padding(
         padding: EdgeInsets.symmetric(horizontal: width * 0.1),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            homeRow(
-              context,
-              Icons.play_arrow,
-              'PLAY',
-              '${level < 2 ? 'Start' : 'Continue'} Your Journey',
-            ),
-            homeRow(
-              context,
-              Icons.event,
-              'STAGES',
-              'Currunt Level: $level',
-            ),
-            homeRow(
-              context,
-              Icons.wallpaper,
-              'BACKGROUNDS',
-              'Currunt: $backgroundStyle',
-            )
-          ],
+        child: FutureBuilder(
+          future: _currnetLevel(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const SizedBox.shrink();
+            }
+            final int level = snapshot.data!;
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                homeRow(
+                  context,
+                  Icons.play_arrow,
+                  'PLAY',
+                  '${level < 2 ? 'Start' : 'Continue'} Your Journey',
+                ),
+                homeRow(
+                  context,
+                  Icons.event,
+                  'STAGES',
+                  'Currunt Level: $level',
+                ),
+                homeRow(
+                  context,
+                  Icons.wallpaper,
+                  'BACKGROUNDS',
+                  'Currunt: $backgroundStyle',
+                )
+              ],
+            );
+          },
         ),
       ),
     );
@@ -57,9 +72,7 @@ class Home extends StatelessWidget {
             height: width * 0.2,
             child: ElevatedButton(
               onPressed: () {
-                if (topic == 'STAGES') {
-                  Navigator.pushNamed(context, Stages.route);
-                }
+                _commonPress(context, topic);
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: colorScheme.secondary,
@@ -76,9 +89,7 @@ class Home extends StatelessWidget {
               splashColor: Theme.of(context).splashColor,
               highlightColor: Theme.of(context).splashColor,
               onTap: () {
-                if (topic == 'STAGES') {
-                  Navigator.pushNamed(context, Stages.route);
-                }
+                _commonPress(context, topic);
               },
               child: FittedBox(
                 child: Column(
@@ -102,5 +113,24 @@ class Home extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void _commonPress(BuildContext context, String topic) async {
+    if (topic == 'STAGES') {
+      Navigator.pushNamed(context, Stages.route);
+    } else if (topic == 'PLAY') {
+      DatabaseFunctions db = DatabaseFunctions();
+      final int curruntLevel = await db.lastUnlockLevel();
+      final int stage = (curruntLevel / 50).ceil();
+      // ignore: use_build_context_synchronously
+      Navigator.pushNamed(
+        context,
+        StageHome.route,
+        arguments: StageHomeArguments(
+          stage,
+          curruntLevel: curruntLevel % 50,
+        ),
+      );
+    }
   }
 }
